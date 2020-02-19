4c528303095f33d42b45be6e9978175bc2f75dcb
Kerem Sevencan <ciukerem@gmail.com>
2020-02-19 18:20:34 +0100

    better layout keys

--- src/Calculator/view/KeyLayout/layout.js
+++ src/Calculator/view/KeyLayout/layout.js
@@ -1,6 +1,25 @@
 import { keyTypes } from "Calculator/logic/constants";
 const { MAIN, BASIC, MEMORY, MATH } = keyTypes;
 
+const EMPTY = ""
+const MAIN_ON = `${MAIN}.on`;
+const MAIN_OFF = `${MAIN}.off`
+const MAIN_CLEAR = `${MAIN}.clear`;
+const MEMORY_CLEAR = `${MEMORY}.clear`
+const MEMORY_RECALL = `${MEMORY}.recall`;
+const MEMORY_MINUS = `${MEMORY}.minus`;
+const MEMORY_PLUS = `${MEMORY}.plus`;
+const BASIC_DIVIDE = `${BASIC}.divide`;
+const BASIC_PERCENTAGE = `${BASIC}.percentage`;
+const BASIC_MULTIPLY = `${BASIC}.multiply`;
+const BASIC_MINUS = `${BASIC}.minus`;
+const BASIC_PLUS = `${BASIC}.plus`;
+const BASIC_PERFORM = `${BASIC}.perform`;
+const MATH_FLOAT = `${MATH}.float`;
+const MATH_SQRT = `${MATH}.sqrt`
+const MATH_CHANGESIGN = `${MATH}.change_sign`;
+
+
 /* Layout
    * .----------------------------------.
    * |      |      |      |   √  |  OFF |
@@ -17,16 +36,10 @@ const { MAIN, BASIC, MEMORY, MATH } = keyTypes;
    * '----------------------------------'
    */
 export default [
-  ["", "", "", `${MATH}.sqrt`, `${MAIN}.off`],
-  [
-    `${MEMORY}.clear`,
-    `${MEMORY}.recall`,
-    `${MEMORY}.minus`,
-    `${MEMORY}.plus`,
-    `${BASIC}.divide`
-  ],
-  [`${BASIC}.percentage`, 7, 8, 9, `${BASIC}.multiply`],
-  [`${MATH}.change_sign`, 4, 5, 6, `${BASIC}.minus`],
-  [`${MAIN}.clear`, 1, 2, 3, `${BASIC}.plus`],
-  [`${MAIN}.on`, 0, `${MATH}.float`, `${BASIC}.perform`]
+  [EMPTY, EMPTY, EMPTY, MATH_SQRT, MAIN_OFF],
+  [MEMORY_CLEAR, MEMORY_RECALL, MEMORY_MINUS, MEMORY_PLUS, BASIC_DIVIDE],
+  [BASIC_PERCENTAGE, 7, 8, 9, BASIC_MULTIPLY],
+  [MATH_CHANGESIGN, 4, 5, 6, BASIC_MINUS],
+  [MAIN_CLEAR, 1, 2, 3, BASIC_PLUS],
+  [MAIN_ON, 0, MATH_FLOAT, BASIC_PERFORM]
 ];
