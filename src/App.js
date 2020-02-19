a2d2c2c1f5c81641d92981765b3731ae958bee65
Kerem Sevencan <kerem@ritmix.org>
2020-02-19 19:36:09 +0100

    put some styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import Calculator from './Calculator'
-import './App.css';
+import React, { Component } from "react";
+import Calculator from "./Calculator";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -8,12 +8,19 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Retro Calculator</h1>
+          <p>
+            Independent memory and profit-margin percent function make this
+            calculator a perfect choice for business use.
+          </p>
+          <p className="App-lead">
+            Casio SL-300SV Solar Powered Standard Function Calculator
+          </p>
         </header>
         <div className="App-intro">
           <Calculator />
         </div>
         <div className="Footer-text">
-        Developed by <strong>Kerem Sevencan</strong>.
+          Developed by <strong>Kerem Sevencan</strong>.
         </div>
       </div>
     );
