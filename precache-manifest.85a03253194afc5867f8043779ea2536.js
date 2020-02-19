3d52554a164fd6494e9def431727195ae3413e81
Kerem Sevencan <kerem@ritmix.org>
2020-02-19 19:16:42 +0100

    Updates

---
+++ precache-manifest.85a03253194afc5867f8043779ea2536.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4593a6f5a9578c1e8e556b7325ef856a",
+    "url": "/retro-calculator/index.html"
+  },
+  {
+    "revision": "9e336422ed2633a0b153",
+    "url": "/retro-calculator/static/css/main.35f43bc2.chunk.css"
+  },
+  {
+    "revision": "a264ab5344c2d137d0f3",
+    "url": "/retro-calculator/static/js/2.4af436b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/retro-calculator/static/js/2.4af436b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e336422ed2633a0b153",
+    "url": "/retro-calculator/static/js/main.f80216ee.chunk.js"
+  },
+  {
+    "revision": "3929c638e72bd19daf8a",
+    "url": "/retro-calculator/static/js/runtime-main.567acea3.js"
+  },
+  {
+    "revision": "fe831f6762cec8fac6f55aaa27fe0fbf",
+    "url": "/retro-calculator/static/media/casio_logo.fe831f67.svg"
+  },
+  {
+    "revision": "9da53ad8a925555b10da48ad5aa0360b",
+    "url": "/retro-calculator/static/media/digit.9da53ad8.woff"
+  },
+  {
+    "revision": "e71bc003a9c545b49ef213af5e527900",
+    "url": "/retro-calculator/static/media/digit.e71bc003.woff2"
+  }
+]);
